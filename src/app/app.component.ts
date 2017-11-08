b4632cc97d412cdeb2c0f55e30405da3fdec8561
Adriel <adrielgusa@gmail.com>
2017-11-08 20:19:17 -0200

    Setting web3 variable in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Web3Service } from './services/web3.service';
 
 @Component({
@@ -6,8 +6,13 @@ import { Web3Service } from './services/web3.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-
+export class AppComponent implements OnInit {
+  title = 'Renthereum';
+  private web3;
   constructor(private web3Service: Web3Service) { }
+
+  ngOnInit() {
+    this.web3Service.init();
+    this.web3 = this.web3Service.getInstance();
+  }
 }
