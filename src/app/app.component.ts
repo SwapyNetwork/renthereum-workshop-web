1a06523964f5bee5c41c47a6742417e0c148fe91
Adriel Santos <adrielgusa@gmail.com>
2017-11-09 08:26:30 -0200

    Send transaction test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,22 @@ export class AppComponent implements OnInit {
   constructor(private web3Service: Web3Service) { }
 
   ngOnInit() {
+    var self = this;
     this.web3Service.init();
     this.web3 = this.web3Service.getInstance();
+    this.web3.eth.getAccounts(console.log);
+    this.web3.eth.getAccounts(function(err, accounts){
+
+      self.web3.eth.sendTransaction({
+        from: accounts[0],
+        to: accounts[1],
+        value: 100000000,
+        gas: 40000
+      }, function(error, response){
+        console.error(error);
+        console.log(response);
+      });
+    
+    });
   }
 }
